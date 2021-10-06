d9ce0b8e417c4e2533451a9102e41839383a5c07
Juan Manuel Perez Toro <juanmaperez@MacBook-Pro-de-Juan.local>
2021-10-06 12:12:21 +0100

    Added projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,22 @@
+import './Projects.css';
 import WorkingProgress from "../WorkingProgress/WorkingProgress";
+import data from "./projects-data";
 
 function Projects() {
     return(
-        <WorkingProgress />
+        // <WorkingProgress />
+        <div className="projects">
+            {data.projects.map(project => {
+                return <div className="project" key={project.title}>
+                    <div className="project-header">
+                        <p className="project-title">{project.title}</p>
+                        <a href={project.github} className="project-url">Github</a>
+                        <a href={project.url} className="project-url">Sitio web</a>
+                    </div>
+                    <p className="project-description">{project.description}</p>
+                </div>
+            })}
+        </div>
     )
 }
 
--- src/components/Projects/Projects.test.js
+++ src/components/Projects/Projects.test.js
@@ -6,3 +6,26 @@ test('renders correctly', () => {
 
     expect(component).toBeTruthy();
 })
+
+test('renders project correctly', () => {
+    const component = render(<Projects />);
+    const project = component.container.querySelector('.project');
+    const projectHeader = component.container.querySelector('.project-header');
+    const projectTitle = component.container.querySelector('.project-title');
+    const projectDescription = component.container.querySelector('.project-description');
+    const projectUrl = component.container.querySelector('.project-url');
+
+    expect(project).toBeTruthy();
+    expect(projectHeader).toBeTruthy();
+    expect(projectTitle).toBeTruthy();
+    expect(projectDescription).toBeTruthy();
+    expect(projectUrl).toBeTruthy();
+})
+
+test.skip('renders download button correctly', () => {
+    const component = render(<Principal />);
+    const downloadButton = component.container.querySelector('.download')
+
+    expect(downloadButton).toBeTruthy();
+    expect(downloadButton).toHaveTextContent('Descarga mi CV')
+})
