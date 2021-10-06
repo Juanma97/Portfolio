c83f0e1d58f650417f9e130e43a7f58c581db5c8
Juan Manuel Perez Toro <juanmaperez@MacBook-Pro-de-Juan.local>
2021-10-06 12:21:02 +0100

    Add files

---
+++ src/components/Projects/projects-data.js
@@ -0,0 +1,24 @@
+const data = {
+    "projects": [
+        {
+            "title": "Budget React App",
+            "description": "Aplicación web hecha en React sobre gestión de finanzas, para aprender sobre la gestión del estado y los diferentes componentes de la librería.",
+            "url": "https://frosty-hypatia-c7b983.netlify.app/",
+            "github": "https://github.com/Juanma97/BudgetReactApp"
+        },
+        {
+            "title": "Appointment React App",
+            "description": "Aplicación web hecha en React sobre gestión de citas, para aprender sobre la librería",
+            "url": "https://sleepy-ptolemy-a08af5.netlify.app/",
+            "github": "https://github.com/Juanma97/AppointmentAppReact"
+        },
+        {
+            "title": "Hexagonal Architecture Example Java/Spring Boot",
+            "description": "Aplicación hecha en Java y Spring Boot a modo de ejemplo para curso impartido sobre arquitectura hexagonal.",
+            "url": "",
+            "github": "https://github.com/Juanma97/ArchitectureHexagonalJava"
+        }
+    ]
+}
+
+export default data;
